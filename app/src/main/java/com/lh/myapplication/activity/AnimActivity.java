6b6077dda38c20a864cc0cb7fb60fff894ec3ff2
liuhang <617847070@qq.com>
2019-12-25 12:09:54 +0800

    1.模仿落体运动

--- app/src/main/java/com/lh/myapplication/activity/AnimActivity.java
+++ app/src/main/java/com/lh/myapplication/activity/AnimActivity.java
@@ -3,12 +3,18 @@ package com.lh.myapplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ValueAnimator;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.lh.myapplication.R;
 import com.lh.myapplication.evaluator.CharEvaluator;
+import com.lh.myapplication.evaluator.FallingBallEvaluator;
+import com.lh.myapplication.interpolator.CustomLinearInterpolator;
 
 public class AnimActivity extends AppCompatActivity {
 
@@ -39,6 +45,25 @@ public class AnimActivity extends AppCompatActivity {
 
         });
 
+
+        ImageView imageView = findViewById(R.id.circle);
+        findViewById(R.id.start_anim).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValueAnimator valueAnimator = ValueAnimator.ofObject(new FallingBallEvaluator(), new Point(0, 0), new Point(400, 400));
+                valueAnimator.setDuration(1000);
+                valueAnimator.start();
+                valueAnimator.setInterpolator(new CustomLinearInterpolator());
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        Point point = (Point)animation.getAnimatedValue();
+                        imageView.layout(point.x,point.y,point.x+imageView.getWidth(),point.y+imageView.getHeight());
+
+                    }
+                });
+            }
+        });
     }
 
 }
